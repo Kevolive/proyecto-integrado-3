42dafbe6a17d93a6d9b2e7ce056538d773b21b06
Victo Polo <viktorpolo16@gmail.com>
2025-03-08 14:12:44 -0500

    creación de getters y setters en todas las entidades

--- src/main/java/com/cesde/proyecto_integrador/models/Perfil.java
+++ src/main/java/com/cesde/proyecto_integrador/models/Perfil.java
@@ -26,6 +26,36 @@ public class Perfil {
         this.telefono = telefono;
         this.urlFoto = urlFoto;
     }
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public LocalDate getFechaNacimiento() {
+        return fechaNacimiento;
+    }
+    public void setFechaNacimiento(LocalDate fechaNacimiento) {
+        this.fechaNacimiento = fechaNacimiento;
+    }
+    public String getDireccion() {
+        return direccion;
+    }
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+    public String getTelefono() {
+        return telefono;
+    }
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+    public String getUrlFoto() {
+        return urlFoto;
+    }
+    public void setUrlFoto(String urlFoto) {
+        this.urlFoto = urlFoto;
+    }
 
     
 }
--- src/main/java/com/cesde/proyecto_integrador/models/grupo.java
+++ src/main/java/com/cesde/proyecto_integrador/models/grupo.java
@@ -23,5 +23,29 @@ public class grupo {
         this.nombre = nombre;
         this.area = area;
     }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
     
 }
